30a1e5d2e086f95d34fae061147e29387a843547
michalshy <91937056+michalshy@users.noreply.github.com>
2024-12-23 21:17:34 +0100

    prepare loop

--- src/engine/file_manager.rs
+++ src/engine/file_manager.rs
@@ -6,30 +6,35 @@ use {bag::Bag, envi::EnvProcessor};
 
 pub struct FileManager {
     bag: Bag,
-    env: EnvProcessor
+    env: EnvProcessor,
+    exit: bool,
 }
 
 impl FileManager {
     pub fn new() -> FileManager {
         let bag = Bag::new();
         let env = EnvProcessor::new();
+        let exit = false;
         FileManager {
             bag,
-            env
+            env,
+            exit
         }
     }
 
     pub fn process(&self) -> Result<()>{
-        let point = match self.env.check_args(1) {
+        let start_point = match self.env.check_args(1) {
             Ok(start) => start,
             Err(e) => return Err(e)
         };
-        self.display(point);
-        Ok(())
+        self.display_first(start_point);
+        while !self.exit {
         
+        }    
+        Ok(())
     }
 
-    fn display(&self, point: String) { 
+    fn display_first(&self, point: String) {
         let paths = fs::read_dir(point.clone()).unwrap();
         println!("path checked: {}", point);
         for path in paths {
@@ -40,5 +45,18 @@ impl FileManager {
                 println!("{}", path.unwrap().path().display());
             }
         }
+    }
+
+    fn display(&self) { 
+        // let paths = fs::read_dir(point.clone()).unwrap();
+        // println!("path checked: {}", point);
+        // for path in paths {
+        //     if let Ok(x) = self.bag.list(path.as_ref().unwrap().path()) {
+        //         println!("{}, size: {}", path.unwrap().path().display(), x);
+        //     }
+        //     else {
+        //         println!("{}", path.unwrap().path().display());
+        //     }
+        // }
     }
 }

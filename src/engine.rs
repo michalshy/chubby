667bbd3bc6a67aabc13c9a8fe1bfe0efc406ad13
Michin <niesmialymichal@gmail.com>
2024-12-24 22:21:20 +0100

    cleanup, prepare entries

--- src/engine.rs
+++ src/engine.rs
@@ -2,7 +2,7 @@ mod file_manager;
 
 pub fn run() {
     match file_manager::FileManager::new().process() {
-        Ok(_) => println!("Process completed"),
+        Ok(_) => println!("Success"),
         Err(e) => println!("Error: {}", e)
     }
 }
--- src/engine/file_manager.rs
+++ src/engine/file_manager.rs
@@ -5,6 +5,7 @@ mod envi;
 use {bag::Bag, envi::EnvProcessor};
 
 pub struct FileManager {
+    elements: Vec<(String, String)>,
     bag: Bag,
     env: EnvProcessor,
     exit: bool,
@@ -14,49 +15,52 @@ impl FileManager {
     pub fn new() -> FileManager {
         let bag = Bag::new();
         let env = EnvProcessor::new();
+        let elements = Vec::new();
         let exit = false;
         FileManager {
+            elements,
             bag,
             env,
             exit
         }
     }
 
-    pub fn process(&self) -> Result<()>{
+    pub fn process(&mut self) -> Result<()>{
         let start_point = match self.env.check_args(1) {
             Ok(start) => start,
             Err(e) => return Err(e)
         };
-        self.display_first(start_point);
+        self.gather(start_point);
+        self.display();
         while !self.exit {
         
         }    
         Ok(())
     }
 
-    fn display_first(&self, point: String) {
+    fn gather(&mut self, point: String) {
         let paths = fs::read_dir(point.clone()).unwrap();
         println!("path checked: {}", point);
         for path in paths {
             if let Ok(x) = self.bag.list(path.as_ref().unwrap().path()) {
-                println!("{}, size: {}", path.unwrap().path().display(), x);
+                let el = 
+                    path.unwrap().path().display().to_string() + 
+                    ", size" +
+                    x.to_string().as_str();
+                self.elements.push((" ".into(), el));
             }
             else {
-                println!("{}", path.unwrap().path().display());
+                let el = 
+                    path.unwrap().path().display().to_string() + 
+                    ", size";
+                self.elements.push((" ".into(), el));
             }
         }
     }
 
     fn display(&self) { 
-        // let paths = fs::read_dir(point.clone()).unwrap();
-        // println!("path checked: {}", point);
-        // for path in paths {
-        //     if let Ok(x) = self.bag.list(path.as_ref().unwrap().path()) {
-        //         println!("{}, size: {}", path.unwrap().path().display(), x);
-        //     }
-        //     else {
-        //         println!("{}", path.unwrap().path().display());
-        //     }
-        // }
+        for (pref, path) in &self.elements {
+            println!("{} - {}", &pref, &path);
+        }
     }
 }
